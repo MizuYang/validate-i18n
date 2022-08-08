cf31c0c77ac866c763d1f57e7b526bdcf7af34de
Mizu <psdk55052004@gmail.com>
2022-08-08 08:25:52 +0800

    修改 自訂驗證函式會先取得當前語言

--- validate/rules.js
+++ validate/rules.js
@@ -7,27 +7,21 @@ const language = {
   'en-US': en,
   'ja-JP': ja
 }
-// //* 取得使用語言
-let lang = localStorage.getItem('lang') || navigator.language
 
-window.addEventListener('storage', () => {
-  console.log('local 變化')
-})
-
-//! 目前先這樣寫，連續去抓目前使用的語言
-//! 若有辦法能夠監聽 localStorage 或在切換語言時執行這支 JS 檔，來讓 lang 變數從 localStorage 抓到目前選擇的語言
-setInterval(() => {
-  lang = localStorage.getItem('lang') || navigator.language
-}, 500)
+function getLang () {
+  return localStorage.getItem('lang') || navigator.language
+}
 
 //* 電話驗證
 export function isPhone (value) {
+  const lang = getLang()
   const phoneNumber = /^(09)[0-9]{8}$/
   const feedback = language[lang].messages.isPhone
   return phoneNumber.test(value) ? true : feedback
 }
 //* 檔案類型驗證
 export function isImage (value) {
+  const lang = getLang()
   const img = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'psd', 'thm', 'yuv', 'ai', 'drw', 'eps', 'ps', 'svg', '3dm', 'max', 'heic']
   const type = value.name.split('.').pop()
   const feedback = language[lang].messages.isImage
